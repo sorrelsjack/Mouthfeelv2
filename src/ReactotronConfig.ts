a41ddc5f8415012a9ed572680799158602377e97
Jack Sorrels <sorrelsjack@gmail.com>
2025-01-08 14:06:37 -0500

    Finished setting up Reactotron properly to display thunks

--- src/ReactotronConfig.ts
+++ src/ReactotronConfig.ts
@@ -1,11 +1,9 @@
 import Reactotron from "reactotron-react-native";
 import { reactotronRedux } from 'reactotron-redux'
 
-// if (__DEV__) {
-//     console.tron = Reactotron;
-// }
-
-Reactotron.configure({ name: 'Mouthfeel' }) // controls connection & communication settings
+const reactotron = Reactotron.configure({ name: 'Mouthfeel' }) // controls connection & communication settings
     .useReactNative()
     .use(reactotronRedux()) // add all built-in react native plugins
     .connect(); // let's connect!
+
+export default reactotron;
--- src/Redux/ConfigureStore.ts
+++ src/Redux/ConfigureStore.ts
@@ -1,9 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose, StoreEnhancer } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import { rootReducer } from './Reducers';
+import Reactotron from '../ReactotronConfig';
+
+const enhancers: StoreEnhancer = compose(applyMiddleware(thunk), Reactotron.createEnhancer())
 
 const logger = createLogger({ collapsed: true });
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(rootReducer, {}, enhancers);
 
 export default store;
